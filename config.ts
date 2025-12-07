474cabee38fa1562b5aec28afe502c34f9211116
Abhiram-advop <abhiramajithr@gmail.com>
2025-12-07 20:29:52 +0530

    Refactor CLI and API structure: Remove Redis integration and related files, consolidate action functions into src directory, and update configuration management.

--- config.ts
+++ config.ts
@@ -3,13 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    redis: {
-        username: String(process.env.REDIS_USERNAME!),
-        password: String(process.env.REDIS_PASSWORD!),
-        host: String(process.env.REDIS_HOST!),
-        port: parseInt(process.env.REDIS_PORT!) 
-    },
-
     server: {
         protocol: process.env.SERVER_PROTOCOL,
         host: process.env.SERVER_HOST,
